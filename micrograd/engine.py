703d8830fef1fb0762cb6d2b9b1d2778886c3c7e
Ajit Nath <nath.ajit01@gmail.com>
2025-11-30 18:45:57 +0800

    Fixes gradient acc and adds backward

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -12,22 +12,23 @@ class Value:
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, _children=(self, other), _op='+')
 
         def _grad():
-            return 1.0
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
-        return Value(
-            data=(self.data + other.data),
-            _children=(self, other),
-            _op='+',
-            _backward=_grad,
-        )
+        out._backward = _grad
+        return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
 
+        out = Value(self.data * other.data, _children=(self, other), _op='*')
+
         def _grad():
-            return other.data
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         
         return Value(
             data=(self.data * other.data),
@@ -38,16 +39,43 @@ class Value:
 
     def __pow__(self, other):
         other = other if isinstance(other, Value) else Value(other)
+
+        out = Value(self.data ** other.data, _children=(self, other), _op=f'**{other}')
         
         def _grad():
-            return other.data * (self.data ** (other.data - 1))
-        
-        return Value(
-            data=(self.data ** other.data),
-            _children=(self, other),
-            _op=f'**{other}',
-            _backward=_grad,
+            self.grad += (
+                other * self.data ** (other - 1)) * out.grad
+        # Ignore other.grad since it's not used in the backward pass
+        out._backward = _grad
+        return out
+
+    def relu(self):
+        out = Value(
+            0 if self.data < 0 else self.data,
+            _children=(self,),
+            _op='ReLU',
         )
+        def _grad():
+            self.grad += (1 if self.data > 0 else 0) * out.grad
+
+        out._backward = _grad
+        return out
+
+    def backward(self):
+        topo: list[Value] = []
+        visited: set[Value] = set()
+
+        def build_topo(v):
+            for child in v._prev:
+                if child not in visited:
+                    build_topo(child)
+            topo.append(v)
+            visited.add(v)
+
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
 
     def __neg__(self):
         return self * -1
